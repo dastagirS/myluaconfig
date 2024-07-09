75df03aa9a65b7ad870d8671d30cd65e37782bdd
dastagirS <shroud464@gmail.com>
2024-07-10 00:12:09 +0530

    add none-ls for code formatters and lsps
    add lackluster dark theme

---
+++ lua/plugins/lackluster.lua
@@ -0,0 +1,12 @@
+return {
+    {
+        "slugbyte/lackluster.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd.colorscheme("lackluster-hack")
+        end
+    }
+
+
+}
